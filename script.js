9935568f08a15911c2e0062921c2a873bee5a281
PersonOOO <umerkhan5110@gmail.com>
2024-06-28 17:17:49 -0500

    site part 1

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.classList.add("shrink");
+    } else {
+        header.classList.remove("shrink");
+    }
+});
+
+
+
